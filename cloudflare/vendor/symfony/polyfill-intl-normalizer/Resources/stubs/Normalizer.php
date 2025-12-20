e1176b7648be904669412e597a86982e7ff7fbec
Justin Gardner <justin@rhynorater.com>
2025-12-20 06:02:06 -0500

    4.14.1

--- cloudflare/vendor/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php
+++ cloudflare/vendor/symfony/polyfill-intl-normalizer/Resources/stubs/Normalizer.php
@@ -1,6 +1,8 @@
 <?php
 
-class Normalizer extends Symfony\Polyfill\Intl\Normalizer\Normalizer
+namespace Cloudflare\APO\Vendor;
+
+class Normalizer extends \Cloudflare\APO\Vendor\Symfony\Polyfill\Intl\Normalizer\Normalizer
 {
     /**
      * @deprecated since ICU 56 and removed in PHP 8
@@ -15,3 +17,4 @@ class Normalizer extends Symfony\Polyfill\Intl\Normalizer\Normalizer
     public const NFC = 16;
     public const NFKC = 32;
 }
+\class_alias('Cloudflare\APO\Vendor\Normalizer', 'Normalizer', \false);
